d751528949c538f20f740f0ec26ad3cda3087e06
HarshCasper <erbeusgriffincasper@gmail.com>
2024-05-19 18:20:57 +0530

    add citi bike data application

---
+++ citi-bike-data-app/app.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const timeout = require('connect-timeout')
+const {PORT = 3000} = process.env
+
+// create express app, set up json parsing and logging
+const app = express();
+app.use(timeout('60s'));
+app.use(express.json());
+app.use(morgan('dev'))
+
+// static assets directory
+app.use(express.static(path.join(__dirname, 'static')));
+
+// custom middleware for parsing querystring params
+const {parseDates,
+       randomizer} = require('./middleware/parse_dates');
+app.use(parseDates);
+app.use(randomizer);
+
+// api routes
+const {getTripsByMonth,
+       getTripsByDayOfWeek,
+       getTripsByTemperature} = require('./routes/api/trips');
+
+// router (homepage)
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+
+// router (api calls)
+app.get('/trips/monthly', getTripsByMonth);
+app.get('/trips/day_of_week', getTripsByDayOfWeek);
+app.get('/trips/temperature', getTripsByTemperature);
+
+// create server
+const server = http.createServer(app);
+server.listen(PORT);
+console.debug('...server listening on port ' + PORT);
---
+++ citi-bike-data-app/models/trip.js
@@ -0,0 +1,79 @@
+const snowflake = require('../database/snowflake.js');
+
+const Trip = function() {}
+
+// query Snowflake to get monthly trip data
+Trip.countByMonth = (start_date, end_date, cb) => {
+  // check if filtering on date range
+  if (start_date && end_date) {
+    var sql = 'select COUNT(*) as trip_count, MONTHNAME(starttime) as month ' +
+      'from trips ' +
+      'where starttime between ? and ? ' +
+      'group by MONTH(starttime), MONTHNAME(starttime) ' +
+      'order by MONTH(starttime);'
+    if ("MATERIALIZED" in process.env) {
+      console.log("...using materialized view COUNT_BY_DAY_MVW");
+      sql = 'select SUM(trip_count) as trip_count, MONTHNAME(starttime) as month ' +
+        'from COUNT_BY_DAY_MVW ' +
+        'where starttime between ? and ? ' +
+        'group by MONTH(starttime), MONTHNAME(starttime) ' +
+        'order by MONTH(starttime);'
+    }
+    snowflake.query(sql, [start_date, end_date]).then(rows => {cb(null, rows)}).catch(err => {cb(err, null)});
+  } else {
+    var sql = 'select COUNT(*) as trip_count, MONTHNAME(starttime) as month ' +
+      'from trips ' +
+      'group by MONTH(starttime), MONTHNAME(starttime) ' +
+      'order by MONTH(starttime);'
+      if ("MATERIALIZED" in process.env) {
+        console.log("...using materialized view COUNT_BY_MONTH_MVW");
+        sql = 'select * from COUNT_BY_MONTH_MVW order by starttime;'
+      }
+    snowflake.query(sql).then(rows => {cb(null, rows)}).catch(err => {cb(err, null)});
+  }
+}
+
+// query Snowflake to get day of week trip data
+Trip.countByDayOfWeek = (start_date, end_date, cb) => {
+  // check if filtering on date range
+  if (start_date && end_date) {
+    var sql = 'select count(*) as trip_count, dayname(starttime) as day_of_week ' +
+            'from trips ' +
+            'where starttime between ? and ? ' +
+            'group by dayofweek(starttime), dayname(starttime) ' +
+            'order by dayofweek(starttime);';
+    snowflake.query(sql, [start_date, end_date]).then(rows => {cb(null, rows)}).catch(err => {cb(err, null)});
+  } else {
+    var sql = 'select count(*) as trip_count, dayname(starttime) as day_of_week ' +
+            'from trips ' +
+            'group by dayofweek(starttime), dayname(starttime) ' +
+            'order by dayofweek(starttime);';
+    snowflake.query(sql).then(rows => {cb(null, rows)}).catch(err => {cb(err, null)});
+  }
+}
+
+Trip.countByTemperature = (start_date, end_date, cb) => {
+  // check if filtering on date range
+  if (start_date && end_date) {
+    var sql = 'with weather_trips as (select * from trips t ' +
+                    'inner join weather w ' +
+                    'on date_trunc("day", t.starttime) = w.observation_date) ' +
+                    'select round(temp_avg_f, -1) as temp, count(*) as trip_count ' +
+                    'from weather_trips ' +
+                    'where starttime between ? and ? ' +
+                    'group by round(temp_avg_f, -1) ' +
+                    'order by round(temp_avg_f, -1) asc;';
+    snowflake.query(sql, [start_date, end_date]).then(rows => {cb(null, rows)}).catch(err => {cb(err, null)});
+  } else {
+    var sql = 'with weather_trips as (select * from trips t ' +
+                    'inner join weather w ' +
+                    'on date_trunc("day", t.starttime) = w.observation_date) ' +
+                    'select round(temp_avg_f, -1) as temp, count(*) as trip_count ' +
+                    'from weather_trips ' +
+                    'group by round(temp_avg_f, -1) ' +
+                    'order by round(temp_avg_f, -1) asc;';
+    snowflake.query(sql).then(rows => {cb(null, rows)}).catch(err => {cb(err, null)});
+  }
+}
+
+module.exports = Trip;
