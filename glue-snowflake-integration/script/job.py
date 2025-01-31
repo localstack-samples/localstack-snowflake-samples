b769744f5eef41dd35deb2470c987b752e58dc19
Przemek Denkiewicz <przemyslaw.denkiewicz@gmail.com>
2025-01-31 16:20:46 +0100

    make it work with the emulator

--- glue-snowflake-integration/script/job.py
+++ glue-snowflake-integration/script/job.py
@@ -1,11 +1,8 @@
-import sys
 from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from py4j.java_gateway import java_import
-from py4j.protocol import Py4JJavaError
 
 SNOWFLAKE_SOURCE_NAME = "net.snowflake.spark.snowflake"
 sc = SparkContext()
@@ -16,18 +13,31 @@ job = Job(glueContext)
 java_import(spark._jvm, SNOWFLAKE_SOURCE_NAME)
 spark._jvm.net.snowflake.spark.snowflake.SnowflakeConnectorUtils.enablePushdownSession(spark._jvm.org.apache.spark.sql.SparkSession.builder().getOrCreate())
 sfOptions = {
-    "sfURL" : "https://dummy.snowflakecomputing.com",
-    "sfUser" : "dummy_user",
-    "sfPassword" : "dummy_password",
-    "sfDatabase" : "dummy_db",
-    "sfSchema" : "dummy_schema",
-    "sfWarehouse" : "dummy_warehouse",
+    "sfURL" : "https://snowflake.localhost.localstack.cloud",
+    "sfUser" : "test",
+    "sfPassword" : "test",
+    "sfDatabase" : "test",
+    "sfSchema" : "public",
+    "sfWarehouse" : "test",
     "application" : "AWSGlue"
 }
 
-## Read from a Snowflake table into a Spark Data Frame
+# Read from a Snowflake table into a Spark Data Frame
 try:
-    df = spark.read.format(SNOWFLAKE_SOURCE_NAME).options(**sfOptions).option("dbtable", "dummy_table").load()
-except Py4JJavaError as e:
+    # Execute select query from table created using init script
+    query = "SELECT * FROM src_glue"
+    df = spark.read \
+        .format(SNOWFLAKE_SOURCE_NAME) \
+        .options(**sfOptions) \
+        .option("query", query) \
+        .load()
+
+    logger.info("Query executed successfully")
+
+    # Show the results
+    df.show()
+
+    job.commit()
+except Exception as e:
     if ("JDBC driver encountered communication error" in str(e)):
         logger.info("JDBC_COMMUNICATION_ERROR")
